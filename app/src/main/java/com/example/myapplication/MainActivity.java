d3dd786531767f0c4af107ae7e71e2bdf70ac2e5
flex2020 <61104736+flex2020@users.noreply.github.com>
2022-06-17 01:14:38 +0900

    test123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     // code 수정
+    // 123
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
